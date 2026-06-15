2c34b0b916ffe0fdbc7f34d6db21f21dd7698ed8
Sumit Kumar Chaudhary <jaiswalsumit1010@gmail.com>
2026-06-15 18:42:50 +0545

    Middleware and RateLimiter completed

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,18 @@
+import ratelimit from "../config/upstash.js";
+
+const rateLimiter = async (req, res, next) => {
+  try {
+    const { success } = await ratelimit.limit(req.ip);
+    if (!success) {
+      return res
+        .status(429)
+        .json({ message: "Too many requests, please try again later." });
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default rateLimiter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
 import {connectDB} from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
@@ -9,13 +10,15 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 
-connectDB();
+
 
 //middleware
-app.use(express.json());
+app.use(express.json()); //this middleware is used to parse incoming JSON requests and make the data available in req.body. It is essential for handling POST and PUT requests where the client sends data in JSON format.
+app.use(rateLimiter); // Apply the rate limiter middleware to all routes
 
 app.use("/api/notes", notesRoutes);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
