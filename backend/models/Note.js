f71d6e1be9916179a91285d9b593505a0b5533f6
sumitchaudhary980 <jaiswalsumit1010@gmail.com>
2026-06-15 10:54:43 +0545

    Database setup completed

---
+++ backend/models/Note.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }, //createdAt and updatedAt fields will be automatically added to the schema
+);
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
