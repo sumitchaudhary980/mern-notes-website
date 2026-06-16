13a25c3b306b76a58b68d2f5a53f85a058f3e158
Sumit Kumar Chaudhary <jaiswalsumit1010@gmail.com>
2026-06-16 09:36:42 +0545

    Fixed cors errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import notesRoutes from './routes/notesRoutes.js';
 import {connectDB} from './config/db.js';
-import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
-
-
 //middleware
 app.use(express.json()); //this middleware is used to parse incoming JSON requests and make the data available in req.body. It is essential for handling POST and PUT requests where the client sends data in JSON format.
 app.use(rateLimiter); // Apply the rate limiter middleware to all routes
+app.use(cors({
+    origin: "http://localhost:5173",
+})); // Enable CORS for all routes
 
 app.use("/api/notes", notesRoutes);
 connectDB().then(() => {
